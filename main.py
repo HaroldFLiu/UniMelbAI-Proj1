bf52d51185d7184778880c19d555a40383e0074a
Harold Liu <harold.liu@student.unimelb.edu.au>
2022-03-31 16:36:03 +1100

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+class Hex:
+    def __init__(self,x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+    def dist(self, other):
+        return Max()
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/

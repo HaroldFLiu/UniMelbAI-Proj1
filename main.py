bc59289c954f7947377130f4cc69e307643f54f2
Harold Liu <harold.liu@student.unimelb.edu.au>
2022-03-31 16:38:45 +1100

    Added project files for pycharm

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 class Hex:
-    def __init__(self,x,y,z):
+    def __init__(self,x,y):
         self.x = x
         self.y = y
-        self.z = z
     def dist(self, other):
         return Max()
 
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    test = Hex(0,0)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

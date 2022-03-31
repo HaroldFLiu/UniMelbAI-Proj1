7fc365bd5e274e74dbfdf7c1c7508e82ad297eac
Harold Liu <harold.liu@student.unimelb.edu.au>
2022-03-31 16:42:22 +1100

    Defined str method for class Hex.

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 class Hex:
-    def __init__(self,x,y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def __str__(self):
+        return f"({self.x},{self.y})"
+
     def dist(self, other):
-        return Max()
-
-
-
-
+        pass
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    test = Hex(0,0)
+    test = Hex(0, 0)
+    print(test)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

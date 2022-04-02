bb398d01765dfcc776f86a016908b992e8819ad7
felix553 <81670336+felix553@users.noreply.github.com>
2022-04-02 16:40:00 +1100

    implemented board size constraint

--- temp.py
+++ temp.py
@@ -2,9 +2,8 @@ import json
 import sys
 
 
-
 class Hex:
-    def __init__(self, x, y, color='n', parent = None):
+    def __init__(self, x, y, color='n', parent=None):
         self.x = x
         self.y = y
         self.color = color
@@ -17,27 +16,35 @@ class Hex:
     def __str__(self):
         return f"({self.x},{self.y})"
 
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.x == other.x and self.y == other.y
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
     def dist(self, other):
         vec_x = (self.x - other.x)
         vec_y = (self.y - other.y)
         return (abs(vec_x) + abs(vec_y) + abs(vec_x + vec_y)) / 2
 
     def neighbor(self):
-        neighbors = [Hex(self.x, self.y + 1), Hex(self.x + 1, self.y)]
+        neighbors = [Hex(self.x, self.y + 1, parent=self), Hex(self.x + 1, self.y, parent=self)]
         if self.x:
-            neighbors.append(Hex(self.x - 1, self.y))
-            neighbors.append(Hex(self.x - 1, self.y+1))
+            neighbors.append(Hex(self.x - 1, self.y, parent=self))
+            neighbors.append(Hex(self.x - 1, self.y + 1, parent=self))
         if self.y:
-            neighbors.append(Hex(self.x, self.y - 1))
-            neighbors.append(Hex(self.x + 1, self.y - 1))
+            neighbors.append(Hex(self.x, self.y - 1, parent=self))
+            neighbors.append(Hex(self.x + 1, self.y - 1, parent=self))
         return neighbors
-    
+
 
 # This is the entry point. class declaration below.
 # Todo: After finishing class construction refactor code so Hex becomes a loadable module.
 
 
-    
 if __name__ == '__main__':
     testing = 0
     if testing:
@@ -52,15 +59,16 @@ if __name__ == '__main__':
     data = json.load(file)
 
     # start working here
-    start = Hex(data["start"][0], data["start"][1], None,)
-    goal = Hex(data["goal"][0], data["goal"][1], None)
+    boardsize = data["n"]
+    start = Hex(data["start"][0], data["start"][1], 'n')
+    goal = Hex(data["goal"][0], data["goal"][1], 'n')
     board = []
     for i in data["board"]:
         board.append(Hex(i[1], i[2], i[0]))
 
-    # debug 
+    # debug
     def astar():
-        openlist = [] 
+        openlist = []
         closed = []
         openlist.append(start)
         current = start
@@ -70,11 +78,12 @@ if __name__ == '__main__':
             for index in range(len(openlist)):
                 if openlist[index].f < openlist[cur_index].f:
                     cur_index = index
-            
+
+            # explore this
             current = openlist[cur_index]
+            #print(current)
             openlist.pop(cur_index)
             closed.append(current)
-
 
             if current == goal:
                 print("Goal Found")
@@ -86,8 +95,14 @@ if __name__ == '__main__':
                 return path
 
             for adjacent_hex in current.neighbor():
-                next = Hex(adjacent_hex.x, adjacent_hex.y, current)
-                neighbor = next
+                neighbor = adjacent_hex
+                # check if hex is on the board
+                if neighbor.x not in range(0, boardsize):
+                    continue
+                if neighbor.y not in range(0, boardsize):
+                    continue
+
+                # if used don't bother
                 if neighbor in board:
                     continue
                 if neighbor not in closed:
@@ -98,22 +113,18 @@ if __name__ == '__main__':
                     else:
                         neighbor.g = temp
                         openlist.append(neighbor)
-                
+
                 neighbor.h = neighbor.dist(goal)
                 neighbor.f = neighbor.g + neighbor.h
-    
-                        
-
 
 
     path = astar()
-
-    print(path)
-
+    if path == None:
+        print(0)
+    else:
+        for i in path[::-1]:
+            print(i)
     
-
-
-
 
 # Done: need to get json parser up and running
 # Done: implement distance calculation between two hex grid
